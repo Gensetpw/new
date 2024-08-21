b021e3b54cecaee067f4e182e59a4f1ef508e7ca
warouhh <136374223+warouhh@users.noreply.github.com>
2024-08-21 22:31:10 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -216,19 +216,19 @@ total_ssh=$(grep -c -E "^### " "/etc/xray/ssh")
 jumlah_noobz=$(grep -c -E "^### " "/etc/xray/noob")
 # TOTAL CREATE ACC TROJAN-GO
 jumlah_trgo=$(grep -c -E "^### " "/etc/trojan-go/trgo")
-function m-ip2(){
-clear
-cd
-if [[ -e /etc/github/api ]]; then
-m-ip
-else
-mkdir /etc/github
-echo "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" > /etc/github/api
-echo "vpnrmbl@gmail.com" > /etc/github/email
-echo "RMBL-VPN" > /etc/github/username
-m-ip
-fi
-}
+#function m-ip2(){
+#clear
+#cd
+#if [[ -e /etc/github/api ]]; then
+#m-ip
+#else
+#mkdir /etc/github
+#echo "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" > /etc/github/api
+#echo "vpnrmbl@gmail.com" > /etc/github/email
+#echo "RMBL-VPN" > /etc/github/username
+#m-ip
+#fi
+#}
 uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
 upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
 uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
