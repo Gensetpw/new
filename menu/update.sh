44f6f8f8a26e948b46beaaa398622b3c30c09ba4
warouhh <136374223+warouhh@users.noreply.github.com>
2024-10-25 00:14:17 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -37,6 +37,7 @@ wget -q -O /usr/bin/trialssh "https://raw.githubusercontent.com/warouhh/new/main
 wget -q -O /usr/bin/autocpu "https://raw.githubusercontent.com/warouhh/new/main/install/autocpu.sh" && chmod +x /usr/bin/autocpu
 wget -q -O /usr/bin/bantwidth "https://raw.githubusercontent.com/warouhh/new/main/install/bantwidth" && chmod +x /usr/bin/bantwidth
 wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/clearlog.sh" && chmod +x /usr/bin/clearlog
+wget -q -O /usr/bin/cleaner "https://raw.githubusercontent.com/warouhh/new/main/install/cleaner.sh" && chmod +x /usr/bin/cleaner
 echo -e " [INFO] Download File Successfully"
 sleep 2
 exit
