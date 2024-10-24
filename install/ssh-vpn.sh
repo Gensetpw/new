381e2832098472c1ea79fce5f0b54b48a2d821dc
warouhh <136374223+warouhh@users.noreply.github.com>
2024-10-25 00:39:59 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -29,6 +29,7 @@ email=none
 curl -sS https://raw.githubusercontent.com/warouhh/new/main/install/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
+sudo apt install iptables-persistent netfilter-persistent
 # go to root
 cd
 
@@ -493,24 +494,25 @@ wget https://raw.githubusercontent.com/warouhh/new/main/install/bbr.sh && chmod 
 
 
 #run_ip
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2095 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2095 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8880 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8880-j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2052 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2052 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 9443 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 9443 -j ACCEPT
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
+apt install iptables-persistent netfilter-persistent
+
+rm -f /etc/iptables.rules && wget -cO - https://pastebin.com/raw/7yc33jRK > /etc/iptables.rules
+
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+iptables -I INPUT -p tcp --dport 80 -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --dport 80 -m state --state NEW -m recent --update --seconds 20 --hitcount 10 -j DROP
+
+
+iptables -I INPUT -p tcp --dport 81 -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --dport 81 -m state --state NEW -m recent --update --seconds 20 --hitcount 10 -j DROP
+
+
+dpkg-reconfigure iptables-persistent
+
+systemctl restart fail2ban
 # blokir torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
 iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
